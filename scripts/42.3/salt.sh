cf64c8fbfab5b02a394cf9a075f0cdcf7d296d6a
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-01-15 08:46:36 +0100

    Fix Salt's repo url for leap 42.3

--- scripts/42.3/salt.sh
+++ scripts/42.3/salt.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-rpm --import http://download.opensuse.org/repositories/systemsmanagement:/saltstack/openSUSE_Leap_42.3/repodata/repomd.xml.key
-zypper addrepo -n 'Systemsmanagement Salt' http://download.opensuse.org/repositories/systemsmanagement:/saltstack/openSUSE_Leap_42.3/ systemsmanagement-salt
+rpm --import http://download.opensuse.org/repositories/systemsmanagement:/saltstack:/products/openSUSE_Leap_42.3/repodata/repomd.xml.key
+zypper addrepo -n 'Systemsmanagement Salt' http://download.opensuse.org/repositories/systemsmanagement:/saltstack:/products/openSUSE_Leap_42.3/ systemsmanagement-salt
 
 zypper --non-interactive --gpg-auto-import-keys in \
   --auto-agree-with-licenses \
-  python
+  python salt salt-minion salt-master
 
 exit 0
