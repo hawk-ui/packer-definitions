7e934f9b0b076d557d807de14622b37068e56701
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-07-18 15:07:25 +0200

    Dev: Install all the packages related to Salt

--- scripts/15.0/salt.sh
+++ scripts/15.0/salt.sh
@@ -3,6 +3,6 @@ set -x
 
 zypper --non-interactive --gpg-auto-import-keys in \
   --auto-agree-with-licenses \
-  salt-minion
+  salt salt-minion salt-master salt-ssh salt-syndic salt-cloud salt-api salt-proxy 
 
 exit 0
