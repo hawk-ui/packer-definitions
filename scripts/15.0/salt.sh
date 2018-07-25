a47762bfea154723cefe4881eeafe6a658765d72
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-07-25 13:03:57 +0200

    Dev: Install only salt-minion so vagrant doesn't complain

--- scripts/15.0/salt.sh
+++ scripts/15.0/salt.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
+rpm --import http://download.opensuse.org/repositories/systemsmanagement:/saltstack:/products/openSUSE_Leap_15.0/repodata/repomd.xml.key
+zypper addrepo -n 'Systemsmanagement Salt' http://download.opensuse.org/repositories/systemsmanagement:/saltstack:/products/openSUSE_Leap_15.0/ systemsmanagement-salt
+
 zypper --non-interactive --gpg-auto-import-keys in \
   --auto-agree-with-licenses \
-  salt salt-minion salt-master salt-ssh salt-syndic salt-cloud salt-api salt-proxy 
+  salt-minion
 
 exit 0
